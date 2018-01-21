dcf7c60c5e9df321b2300399e8dad9ebf8b8775e
DESKTOP-A7J5F0K\bishoy <bishoy.shaheer@gmail.com>
2018-01-21 16:15:03 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const fs = require('fs');
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+//middle ware for logging( like filter )
+app.use((req, res, next) => {
+  var now = new Date();
+  var log = now + " " + req.method + " " + req.url + "\n";
+  console.log(log);
+  fs.appendFileSync('server.log', log, (error) => {
+    console.log('can\'t write to file', error);
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.send({
+//     status: 'UNDER_MAINTENANCE'
+//   });
+// });
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello Express!</h1>');
+});
+
+app.get('/about', (req, res) =>{
+  res.send({
+    name: 'bishoy',
+    likes: [
+      'reading',
+      'travelling'
+    ]
+  });
+});
+
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'bad request.'
+  });
+});
+
+app.listen(3000);
